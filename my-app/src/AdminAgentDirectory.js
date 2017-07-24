e1514e0d857b326cdb09302178453e6b68f6b142
Annie Sharkey <anniesharkey@students-mbp-3.lan>
2017-07-24 14:24:07 -0400

    adding stuff

---
+++ my-app/src/AdminAgentDirectory.js
@@ -0,0 +1,203 @@
+import React, { Component } from "react";
+import { Table, Tabs, Button, Input, Icon } from "antd";
+import "antd/dist/antd.css";
+import axios from "axios";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import './AdminDirectory.css';
+const TabPane = Tabs.TabPane;
+
+export default class AdminAgentDirectory extends Component {
+  state = {
+    filterDropdownVisible: false,
+    data: [],
+    officeData: [],
+    searchText: "",
+    filtered: false,
+    searchData: []
+  };
+  onInputChange = e => {
+    this.setState({ searchText: e.target.value });
+  };
+  onSearch = () => {
+    const { searchText } = this.state;
+    const reg = new RegExp(searchText, "gi");
+    this.setState({
+      filterDropdownVisible: false,
+      filtered: !!searchText,
+      searchData: this.state.officeData
+        .map(record => {
+          const match = record.agentName.match(reg);
+          if (!match) {
+            return null;
+          }
+          return {
+            ...record,
+            name: (
+              <span>
+                {record.agentName.split(reg).map(
+                  (text, i) =>
+                    i > 0
+                      ? [
+                          <span className="highlight">
+                            {match[0]}
+                          </span>,
+                          text
+                        ]
+                      : text
+                )}
+              </span>
+            )
+          };
+        })
+        .filter(record => !!record)
+    });
+  };
+
+  componentWillMount() {
+    axios.get("http://localhost:4000/api/agents").then(res => {
+      this.setState({
+        data: res.data,
+        officeData: res.data.filter(agent => {
+          return "Asheville" === agent.agentOffice;
+        })
+      });
+    });
+  }
+
+  handleTabClick(key) {
+    var data = this.state.data;
+    this.setState({
+      officeData: data.filter(agent => {
+        return key === agent.agentOffice;
+      })
+    });
+  }
+
+  handleTabClick(key) {
+    var data = this.state.data;
+    this.setState({
+      officeData: data.filter(agent => {
+        return key === agent.agentOffice;
+      }),
+      searchData: [],
+      filtered: false,
+      searchText: ""
+    });
+  }
+
+  render() {
+    let { sortedInfo } = this.state;
+    sortedInfo = sortedInfo || {};
+    const columns = [
+      {
+        title: "Agent Name",
+        dataIndex: "agentName",
+        width: 150,
+        sorter: function(a, b) {
+          var nameA = a.agentName.toLowerCase(),
+            nameB = b.agentName.toLowerCase();
+          if (
+            nameA < nameB //sort string ascending
+          )
+            return -1;
+          if (nameA > nameB) return 1;
+          return 0; //default return value (no sorting)
+        },
+        filterDropdown: (
+          <div className="custom-filter-dropdown">
+            <Input
+              ref={ele => (this.searchInput = ele)}
+              placeholder="Search name"
+              value={this.state.searchText}
+              onChange={this.onInputChange}
+              onPressEnter={this.onSearch}
+            />
+            <Button type="primary" onClick={this.onSearch}>
+              Search
+            </Button>
+          </div>
+        ),
+        filterIcon: (
+          <Icon
+            type="search"
+            style={{ color: this.state.filtered ? "#108ee9" : "#aaa" }}
+          />
+        ),
+        filterDropdownVisible: this.state.filterDropdownVisible,
+        onFilterDropdownVisibleChange: visible => {
+          this.setState(
+            {
+              filterDropdownVisible: visible
+            },
+            () => this.searchInput.focus()
+          );
+        }
+      },
+      {
+        title: "Code",
+        dataIndex: "agentCode",
+        width: 150,
+        sorter: function(a, b) {
+          var nameA = a.agentCode.toLowerCase(),
+            nameB = b.agentCode.toLowerCase();
+          if (
+            nameA < nameB //sort string ascending
+          )
+            return -1;
+          if (nameA > nameB) return 1;
+          return 0; //default return value (no sorting)
+        }
+      },
+      {
+        title: "Email",
+        dataIndex: "agentEmail",
+        width: 150
+      },
+      {
+        title: "Phone",
+        dataIndex: "agentPhoneNumber",
+        width: 150
+      },
+      {
+        title: "Edit",
+        key: "action",
+        render: (text, record) =>
+          <span>
+            <Icon type="edit" />
+          </span>
+      }
+    ];
+
+    return (
+      <div>
+        <div className="header">
+        <Link to="/">
+          <Icon type="arrow-left" style={{ fontSize: 30 }} />
+        </Link>
+        <h1 >Agent Directory</h1>
+        </div>
+
+        <Tabs
+          defaultActiveKey="Asheville"
+          onChange={key => this.handleTabClick(key)}
+        >
+          <TabPane tab="Asheville" key="Asheville" />
+          <TabPane tab="Charlottesville" key="Charlottesville" />
+          <TabPane tab="Fredericksburg" key="Fredericksburg" />
+          <TabPane tab="New River Valley" key="New River Valley" />
+          <TabPane tab="Richmond" key="Richmond" />
+          <TabPane tab="Shenandoah Valley" key="Shenandoah Valley" />
+          <TabPane tab="Wilmington" key="Wilmington" />
+        </Tabs>
+        <Table
+          columns={columns}
+          dataSource={
+            this.state.filtered ? this.state.searchData : this.state.officeData
+          }
+          pagination={{ pageSize: 50 }}
+          // scroll={{ y: 240 }}
+        />
+      </div>
+    );
+  }
+}
